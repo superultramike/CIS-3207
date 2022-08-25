1aa69f9f572cc5822b068feea70b5a18eda9d241
superultramike11 <tuj25271@temple.edu>
2022-08-24 20:07:15 -0400

    Progress
    Got tuls to scan the contents of the current directory and determine if the item it's looking at is a file or directory.

---
+++ tuls.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+// Prototype for recursive function
+// void recursive_dir(int indent, char * pathname);
+
+int main(int argc, char **argv) {
+    struct dirent **namelist;
+    struct stat sb;
+    int n;
+
+    // print the # of arguments passed to program
+    //printf("Argv[1]: %s\n", argv[1]);
+    //printf("Number of arguments: %d\n", argc);
+
+    //print out the string for each argument
+    /*
+    for(int i=0; i<argc; i++) {
+        printf("Argument: %s\n", argv[i]);
+    }
+
+    stat(argv[0], &sb);
+    if(S_ISREG(sb.st_mode)) {
+        printf("This is a file\n");
+    }
+    */
+
+    n = scandir(".", &namelist, NULL, alphasort);
+    //printf("Number of Files/directs total: %d\n", n);
+    if (n == -1) {
+        perror("scandir");
+        exit(EXIT_FAILURE);
+    }
+
+    while (n--) {
+        //printf("%s\n", namelist[n]->d_name);
+        //printf("Number of Files/directs left %d\n", n);
+        stat(namelist[n]->d_name, &sb);
+        if(S_ISREG(sb.st_mode)) {
+            printf("%s is a file\n", namelist[n]->d_name);
+        }
+        if(S_ISDIR(sb.st_mode)) {
+            printf("%s is a directory\n", namelist[n]->d_name);
+        }
+        free(namelist[n]);
+    }
+    free(namelist);
+
+    exit(EXIT_SUCCESS);
+}
