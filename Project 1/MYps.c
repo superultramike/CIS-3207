f51e066143a92a89e5f555f14c398148e0c670c7
superultramike11 <tuj25271@temple.edu>
2022-09-22 18:14:47 -0400

    small formatting changes

--- Project 1/MYps.c
+++ Project 1/MYps.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+void printStime(int pid);
 void printUtime(int pid);
 void printStat(int pid);
 void printSize(int pid);
@@ -14,19 +15,19 @@ int main(int argc, char *argv[]) {
     int cflag = 0;
     int uFlag = 0;
 
-    while((opt = getopt(argc, argv, "p:sUSvc")) != -1) {
+    while((opt = getopt(argc, argv, "p:s:U:S:v:c:")) != -1) {
         switch(opt) {
             // NOT DONE
             case 'p':
-                printf("(p) Display process information only for the process whose number is pid.\n");
+                //printf("(p) Display process information only for the process whose number is pid.\n");
                 pid = atoi(optarg);
-                printf("PID given = %d\n", pid);
+                printf("PID: %d ", pid);
                 break;
 
             // DONE
             // using the given user pid, open the specific proc stat file for given pid
             case 's':
-                printf("(s) Display the single-character state information about the process.\n");
+                //printf("(s) Display the single-character state information about the process.\n");
                 printStat(pid);
                 break;
 
@@ -34,33 +35,34 @@ int main(int argc, char *argv[]) {
             // DONE
             // If U is present (uflag = 1) don't display the user timer, if not present then display user time
             case 'U':
-                printf("(U) DO NOT Display the amount of user time consumed by this process\n");
+                //printf("(U) DO NOT Display the amount of user time consumed by this process\n");
                 uFlag = 1;
                 break;
 
-            // NOT DONE
+            // DONE
             // Use strtok to parse through to the 15th element "stime" (INTERMEDIATE);
             case 'S':
-                printf("(S) Display the amount of system time consumed so far by this process\n");
+                //printf("(S) Display the amount of system time consumed so far by this process\n");
+                printStime(pid);
                 break;
 
             // DONE
             // Use the fscanf method to capture this data (EASY)
             case 'v':
-                printf("(v) Display the amount of virtual memory currently used (in pages) by this program\n");
+                //printf("(v) Display the amount of virtual memory currently used (in pages) by this program\n");
                 printSize(pid);
                 break;
 
             // DONE
             // Make 'c' a flag case so that if it's true it doesn't run the print statement below this case
             case 'c':
-                printf("(c) DO NOT Display the command line that started this program\n");
+                //printf("(c) DO NOT Display the command line that started this program\n");
                 cflag = 1;
                 break;
         }
     }
 
-    // NOT DONE
+    // DONE
     if(uFlag == 0) {
         // make a function inside here
         printUtime(pid);
@@ -71,12 +73,13 @@ int main(int argc, char *argv[]) {
         printCmdline(pid);
     }
 
+    printf("\n");
+
     for(; optind < argc; optind++) {
         printf("extra arguments: %s\n", argv[optind]);
     }
 }
 
-// FIX THIS prints seg fault
 void printUtime(int pid) {
     // Declare variables to print out stat menu
     char filename[1000];
@@ -98,7 +101,36 @@ void printUtime(int pid) {
             token = strtok(NULL, s);
             tokenCount++;
             if(tokenCount == 13) {
-                printf("UTIME: %s\n", token);
+                printf(" UTIME: %s ", token);
+                break;
+            }
+        }
+    }
+    fclose(f);
+}
+
+void printStime(int pid) {
+    // Declare variables to print out stat menu
+    char filename[1000];
+    sprintf(filename, "/proc/%d/stat", pid);
+    FILE *f = fopen(filename, "rb");
+    char *arg;
+    size_t size = 0;
+    while(getdelim(&arg, &size, 0, f) != -1) {
+        // print out the entire contents of stat
+        //printf("%s\n", arg);
+        const char s[2] = " ";
+        char *token;
+        int tokenCount = 0;
+        token = strtok(arg, s);
+
+        // walk through the other tokens
+        while(token != NULL) {
+            //printf("%s\n", token );
+            token = strtok(NULL, s);
+            tokenCount++;
+            if(tokenCount == 14) {
+                printf(" STIME: %s ", token);
                 break;
             }
         }
@@ -114,7 +146,7 @@ void printCmdline(int pid) {
     char comm[1000];
     fscanf(f, "%s", comm);
     //printf("DO Display the command line that started this program\n");
-    printf("cmdline: %s\n", comm);
+    printf(" cmdline: %s ", comm);
     fclose(f);
 }
 
@@ -126,7 +158,7 @@ void printStat(int pid) {
     int unused, ppid;
     char comm[1000], state;
     fscanf(f, "%d %s %c %d", &unused, comm, &state, &ppid);
-    printf("State = %c\n", state);
+    printf(" State: %c ", state);
     fclose(f);
 }
 
@@ -137,6 +169,6 @@ void printSize(int pid) {
 
     int size;
     fscanf(f, "%d", &size);
-    printf("Size = %d\n", size);
+    printf(" Size: %d ", size);
     fclose(f);
 }
