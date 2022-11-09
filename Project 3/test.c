43cfd3d3ef363c0367c15e428ff0488e3e206210
superultramike11 <tuj25271@temple.edu>
2022-11-08 22:45:37 -0500

    YES

--- Project 3/test.c
+++ Project 3/test.c
@@ -6,19 +6,18 @@
 #include<sys/wait.h>
 
 typedef struct {
-    int count;
-    int prod_num;
-} prod;
+    int type;
+} product;
 
 // write info into the left side of the pipe whose write-end is given by pipe_write_end
 void producer1(FILE *pipe_write_end) {
-    prod *args = (prod *) args;
-    args->prod_num = 1;
+    product *args = (product *) args;
+    args->type = 1;
     // produce 10 elements
-    for((args->count)=0; (args->count)<= 10; (args->count)++) {
+    for(int i=0; i<= 10; i++) {
         // send each element i through the left side stream of the pipe
-        fprintf(pipe_write_end, "%d %d", args->count, args->prod_num);
-        printf("Prod_num %d made #%d element\n", args->prod_num, args->count);
+        fprintf(pipe_write_end, "%d ", args->type);
+        printf("Prod type = %d\n", args->type);
     }
     // close the hatch since we are done
     fclose(pipe_write_end);
@@ -28,13 +27,13 @@ void producer1(FILE *pipe_write_end) {
 
 // write info into the left side of the pipe whose write-end is given by pipe_write_end
 void producer2(FILE *pipe_write_end) {
-    prod *args = (prod *) args;
-    args->prod_num = 2;
-    // produce 100 elements
-    for((args->count)=0; (args->count)<= 10; (args->count)++) {
+    product *args = (product *) args;
+    args->type = 2;
+    // produce 10 elements
+    for(int i=0; i<= 10; i++) {
         // send each element i through the left side stream of the pipe
-        fprintf(pipe_write_end, "%d %d", args->count, args->prod_num);
-        printf("Prod_num %d made #%d element\n", args->prod_num, args->count);
+        fprintf(pipe_write_end, "%d ", args->type);
+        printf("Prod type = %d\n", args->type);
     }
     // close the hatch since we are done
     fclose(pipe_write_end);
@@ -58,7 +57,7 @@ void consumer(FILE *pipe_read_end) {
 }
 
 int main() {
-    prod args = {0, 0};
+    product args = {0};
     int producer1_id, producer2_id, consumer_id;
     int pd[2];
     FILE *pipe_write_end, *pipe_read_end;
@@ -96,14 +95,16 @@ int main() {
         producer2(pipe_write_end);
     }
 
-    // fork the consumer
-    consumer_id = fork();
-    // child process
-    if(consumer_id == 0) {
-        // close the left side of the pipe since we are reading new data
-        fclose(pipe_write_end);
-        // call the consumer function to read the new data
-        consumer(pipe_read_end);
+    product p;
+
+    int k, m;
+    while(1) {
+        // read the right side of the pipe with fscanf and declare it as a variable to be used for STDOUT
+        printf("Retreived type: %d\n", p.type);
+        int n = fscanf(pipe_read_end, "%d", &k);
+        // If fscanf was successful
+        if(n == 1) printf("consumer got %d\n", k);
+        else exit(0);
     }
 
     // Close each side of the pipe and wait for each child process to finish
